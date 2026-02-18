27df2c2f43e470f943e9aeaae22d2b0163df73f8
Manuele79 <manuele.martino@gmail.com>
2026-02-19 00:15:39 +0100

    nero

--- app/components/EventQr.tsx
+++ app/components/EventQr.tsx
@@ -119,7 +119,8 @@ export default function EventQr({ eventCode }: { eventCode: string }) {
       </div>
 
 
-     <div className="rounded-2xl border border-yellow-400/15 bg-white/10 p-3 print:border-0 print:bg-transparent print:p-0">
+    <div className="rounded-2xl border border-yellow-400/40 bg-black p-4 print:border-0 print:bg-transparent print:p-0">
+
      <div className="print-only">
      \ {printPng ? (
     <img src={printPng} alt="QR" style={{ width: 360, height: 360 }} />
