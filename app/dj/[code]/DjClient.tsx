c3a8c1f2842811d2ac551fd2a7c462aaa782a105
Manuele79 <manuele.martino@gmail.com>
2026-03-14 11:54:54 +0100

    spazio2

--- app/dj/[code]/DjClient.tsx
+++ app/dj/[code]/DjClient.tsx
@@ -438,7 +438,7 @@ function toggleSelect(id: string) {
       <div className="pointer-events-none absolute -top-48 left-1/2 h-[600px] w-[600px] -translate-x-1/2 rounded-full bg-emerald-400/20 blur-[140px]" />
       <div className="pointer-events-none absolute -bottom-56 right-[-160px] h-[600px] w-[600px] rounded-full bg-pink-400/20 blur-[140px]" />
 
-      <div className="mx-auto max-w-6xl px-4 py-8">
+      <div className="mx-auto max-w-[1600px] px-4 py-8">
         {/* HEADER TOP */}
         <div className="mb-6 flex flex-col gap-4 md:gap-8">
           <div className="flex flex-wrap items-center gap-3">
@@ -642,9 +642,9 @@ function toggleSelect(id: string) {
 
 
         {/* MAIN GRID */}
-        <div className="grid grid-cols-1 gap-5 2xl:grid-cols-3">
+        <div className="grid grid-cols-1 gap-5 lg:grid-cols-3">
           {/* LEFT */}
-          <div className="2xl:col-span-2">
+          <div className="lg:col-span-2">
             {mode === "party" ? (
               <section className="rounded-3xl border border-yellow-400/40 bg-zinc-950/70 shadow-[0_0_35px_rgba(253,224,71,0.35)] p-2 sm:p-3 ">
                 <div className="mb-2 flex flex-wrap items-center justify-between gap-2 px-4 text-xs text-cyan-400">
@@ -933,7 +933,7 @@ function toggleSelect(id: string) {
  </div>
 
           {/* RIGHT: QR */}
-          <aside className="2xl:col-span-1">
+          <aside className="lg:col-span-1">
             <div className="sticky top-4 p-[1px] rounded-3xl overflow-hidden bg-gradient-to-br from-zinc-900/80 via-zinc-900/70 to-zinc-900/80 shadow-[0_10px_30px_rgba(0,0,0,0.25)]">
 
              <div className="rounded-3xl 
