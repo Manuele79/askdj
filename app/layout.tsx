57b13b61758590df7c39fc01acc6c6cc6cec2934
Manuele79 <manuele.martino@gmail.com>
2026-05-31 23:18:31 +0200

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata, Viewport } from "next";
 import ServiceWorkerRegister from "./ServiceWorkerRegister";
 
 import { Geist, Geist_Mono } from "next/font/google";
-import "../app/globals.css";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
